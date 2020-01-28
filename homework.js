87daa03977399ce45292f1dc36215631c26a1a80
odyssic <stevenbuddenjr@gmail.com>
2020-01-28 18:22:32 -0500

    homework through 5

--- homework.js
+++ homework.js
@@ -2,21 +2,94 @@
 // member of the array, and returns a new array with that member removed.
 // For example, `remove(['Cadence', 'Ordel', 'Marion'], 'Marion')` results
 // in `['Cadence', 'Ordel']`.
+
 //
 // If the potential member is not in the array, return the array unchanged.
 // If the potential member is in the array, remove all instances of it from the array.
 
+function remove(names, name) {
+    while (names.includes(name)) {
+        let i = names.indexOf(name);
+        names.splice(i, 1);
+    }
+    return names;
+}
+
 // 2. Revisit your "remove" function. Make sure that it does not change the original
 // array but instead returns a new array.
+
+function remove(names, name) {
+    let newarray = names.slice();
+    while (newarray.includes(name)) {
+        let i = newarray.indexOf(name);
+        newarray.splice(i, 1);
+    }
+    return newarray;
+}
+
+//newarray = arr.splice() *for instance
 
 // 3. Create a function called "sum" that takes an array of numbers and
 // returns the sum of those numbers.
 
+function sum(numbers) {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i];
+    }
+    return sum;
+}
+
+// Let scores = [1,2,3,4,5,]
+// Let sum = scores.reduce(function(total, score){return total + score} , 0)
+
+// function sum()
+// returns total.sum
+
+// function sum(list) {
+//     return total.sum(list);
+// }
+
+// int arraySum(int[] array) {
+//     int sum = 0;
+//     for (int i = 0; i <= array.length; i++) {
+//         sum = sum + array[i];
+//     }
+
+//     return sum;
+// }
+
 // 4. Create a function called "average" that takes an array of numbers
 // and returns the average of those numbers.
 
+function average(numbers) {
+    if (numbers.length > 0) {
+        let sum = 0;
+        for (let i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        return sum / numbers.length;
+    }
+}
+
 // 5. Create a function called "minimum" that takes an array of numbers and
 // returns the smallest number in that array.
+
+// function minimum(array) {
+
+// }
+
+//Isn’t that a nice looking line of code, Math.min.apply(null, numbers)?
+
+//It’s pretty to look at and very easy to understand. Math.min(a, b, c[, ...])
+
+function minimum(numbers) {
+    if (numbers.length > 0) {
+        return Math.min(numbers);
+    }
+}
+
+//spread operator
 
 // 6. There are many techniques to sort arrays in programming. Your programming
 // language will likely include the ability to do this. We are going to
@@ -46,4 +119,4 @@
 // into a string separated by commas.
 //
 // For example, `textList(['Cadence', 'Ordel', 'Marion'])` results in the string
-// `"Cadence,Ordel,Marion"`.
+// `"Cadence,Ordel,Marion"`
